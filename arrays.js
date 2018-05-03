8942747ed0c4d4e76725f22952f8c4219941a02b
Chris Gioeli <cgioeli@gmail.com>
2018-05-03 16:31:17 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -31,7 +31,7 @@ function removeElementFromBeginningOfArray(chocolateBars){
     return chocolateBars
   }
   function removeElementFromEndOfArray(chocolateBars){
-    chocolateBars.slice(0, chocolateBars.length - 2)
+    chocolateBars.slice(0, chocolateBars.length - 1)
     return chocolateBars
   }
   
